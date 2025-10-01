8bf5737db70236a5dc6ae1750f37f2cbb0d5541f
husham-99 <hushamahmed388@gmail.com>
2025-10-01 08:04:22 -0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,25 +1,19 @@
 // check if there is localStorage color option
 
-const mainColors = localStorage.getItem('color-option')
-if(mainColors != null){
+const mainColors = localStorage.getItem("color-option");
+if (mainColors != null) {
+  document.documentElement.style.setProperty("--main-color", mainColors);
 
-    document.documentElement.style.setProperty('--main-color',mainColors)
-    
-    // check for active class
-    //remove active class from all colors list itemmainColors
-    document.querySelectorAll('.colors-list li').forEach(element => {
-            
-        element.classList.remove('active')
+  // check for active class
+  //remove active class from all colors list itemmainColors
+  document.querySelectorAll(".colors-list li").forEach((element) => {
+    element.classList.remove("active");
 
-         // add active class on element with data-color == local Storage item
-         if(element.dataset.color === mainColors){
-
-            element.classList.add('active')
-         }
-    });
-   
-
-
+    // add active class on element with data-color == local Storage item
+    if (element.dataset.color === mainColors) {
+      element.classList.add("active");
+    }
+  });
 }
 
 // random background option
@@ -28,329 +22,256 @@ let BackgroundOption = true;
 // variable to control the intervel
 let theInterval;
 
-// check if there is local storage random background item 
-let backgroundLocalItem = localStorage.getItem('background-option')
+// check if there is local storage random background item
+let backgroundLocalItem = localStorage.getItem("background-option");
 // check if random background localStorage is not empty
-if(backgroundLocalItem != null){
+if (backgroundLocalItem != null) {
+  if (backgroundLocalItem == "true") {
+    BackgroundOption = true;
+  } else {
+    BackgroundOption = false;
+  }
 
-    
-    if(backgroundLocalItem == 'true'){
+  // remove active class from all button
+  document.querySelectorAll(".button button").forEach((element) => {
+    element.classList.remove("active");
+  });
 
-        BackgroundOption = true
-    }else{
-
-        BackgroundOption = false
-    }
-    
-
-    // remove active class from all button
-    document.querySelectorAll('.button button').forEach(element => {
-        
-        element.classList.remove('active')
-    });
-
-    if(backgroundLocalItem == 'true'){
-
-        document.querySelector('.button .yes').classList.add('active')
-    }else{
-
-        document.querySelector('.button .no').classList.add('active')
-    }
+  if (backgroundLocalItem == "true") {
+    document.querySelector(".button .yes").classList.add("active");
+  } else {
+    document.querySelector(".button .no").classList.add("active");
+  }
 }
 // open and close nav
 
-function openCloseNav(){
+function openCloseNav() {
+  let nav = document.getElementById("nav");
+  let menu = document.getElementById("menu");
 
-    let nav = document.getElementById('nav')
-    let menu = document.getElementById('menu')
-    
-    document.addEventListener('scroll', function (){
+  document.addEventListener("scroll", function () {
+    nav.classList.remove("active");
+    menu.classList.remove("active");
+    document.querySelector(".overlay").style.display = "none";
+    document.querySelector(".clock").style.visibility = "visible";
+    document.querySelector(".setting-box").style.visibility = "visible";
+  });
+  menu.addEventListener("click", function (e) {
+    // stop propagation
+    e.stopPropagation();
 
-        nav.classList.remove('active')
-        menu.classList.remove('active')
-        document.querySelector('.overlay').style.display = "none"
-        document.querySelector('.clock').style.visibility = "visible"
-        document.querySelector('.setting-box').style.visibility = "visible"
+    nav.classList.toggle("active");
+    this.classList.toggle("active");
 
-    })
-    menu.addEventListener('click', function(e){
-
-        // stop propagation
-        e.stopPropagation()
-
-        nav.classList.toggle('active')
-        this.classList.toggle('active')
-
-        if(nav.classList.contains('active')){
-
-            document.querySelector('.clock').style.visibility = "hidden"
-            document.querySelector('.setting-box').style.visibility = "hidden"
-            document.querySelector('.overlay').style.display = "block"
-        }else{
-
-            document.querySelector('.clock').style.visibility = "visible"
-            document.querySelector('.setting-box').style.visibility = "visible"
-            document.querySelector('.overlay').style.display = "none"
-        }
-        
-    })
-    
-    
-
+    if (nav.classList.contains("active")) {
+      document.querySelector(".clock").style.visibility = "hidden";
+      document.querySelector(".setting-box").style.visibility = "hidden";
+      document.querySelector(".overlay").style.display = "block";
+    } else {
+      document.querySelector(".clock").style.visibility = "visible";
+      document.querySelector(".setting-box").style.visibility = "visible";
+      document.querySelector(".overlay").style.display = "none";
+    }
+  });
 }
-openCloseNav()
+openCloseNav();
 
 // click anywhere outside nav and toggle menu
 
-document.addEventListener('click', (e) =>{
-
-    if(e.target !== menu && e.target !== nav){
-
-        nav.classList.remove('active')
-        menu.classList.remove('active')
-        document.querySelector('.clock').style.visibility = "visible"
-        document.querySelector('.setting-box').style.visibility = "visible"
-        document.querySelector('.overlay').style.display = "none"
-    }else{
-        document.querySelector('.overlay').style.display = "block"
-
-    }
-    
-})
+document.addEventListener("click", (e) => {
+  if (e.target !== menu && e.target !== nav) {
+    nav.classList.remove("active");
+    menu.classList.remove("active");
+    document.querySelector(".clock").style.visibility = "visible";
+    document.querySelector(".setting-box").style.visibility = "visible";
+    document.querySelector(".overlay").style.display = "none";
+  } else {
+    document.querySelector(".overlay").style.display = "block";
+  }
+});
 
 // stop propagation on nav
-nav.onclick = function(e){
-
-    e.stopPropagation()
-}
-
+nav.onclick = function (e) {
+  e.stopPropagation();
+};
 
 // open and close setting
 
-function openCloseGear(){
+function openCloseGear() {
+  let setting = document.getElementById("setting");
 
-    let setting = document.getElementById('setting')
+  setting.classList.toggle("active");
 
-    setting.classList.toggle('active')
-
-    if(setting.classList.contains('active')){
-
-        
-        document.querySelector('.overlay').style.display = "block"
-    }else{
-
-        
-        document.querySelector('.overlay').style.display = "none"
-    }
+  if (setting.classList.contains("active")) {
+    document.querySelector(".overlay").style.display = "block";
+  } else {
+    document.querySelector(".overlay").style.display = "none";
+  }
 }
-
 
 // switch colors
 
-let colorsList = document.querySelectorAll('.colors-list li')
+let colorsList = document.querySelectorAll(".colors-list li");
 // loop on all list items
-colorsList.forEach(li => {
-    // click on every list item 
-    li.addEventListener('click', (e) =>{
+colorsList.forEach((li) => {
+  // click on every list item
+  li.addEventListener("click", (e) => {
+    // set color on root
+    document.documentElement.style.setProperty(
+      "--main-color",
+      e.target.dataset.color
+    );
 
-         
+    // set color on localStorage
+    localStorage.setItem("color-option", e.target.dataset.color);
 
-        // set color on root
-        document.documentElement.style.setProperty('--main-color' ,  e.target.dataset.color)
-
-        // set color on localStorage
-        localStorage.setItem('color-option', e.target.dataset.color)
-
-        //remove active class from all children
-        e.target.parentElement.querySelectorAll('.active').forEach(element => {
-            
-            element.classList.remove('active')
-
-             
-            
-        });
-
-        // add active class on self
-        e.target.classList.add('active')
- 
+    //remove active class from all children
+    e.target.parentElement.querySelectorAll(".active").forEach((element) => {
+      element.classList.remove("active");
     });
-})
 
+    // add active class on self
+    e.target.classList.add("active");
+  });
+});
 
 // switch random background option
 
-let randomBackGroundEl = document.querySelectorAll('.button button')
+let randomBackGroundEl = document.querySelectorAll(".button button");
 // loop on all list buttons
-randomBackGroundEl.forEach(button => {
-    // click on every button
-    button.addEventListener('click', (e) =>{
-
-        
-
-        //remove active class from all buttons
-        e.target.parentElement.querySelectorAll('.active').forEach(element =>{
-            
-            element.classList.remove('active')
-
-        });
-         
-            
-        // add active class on self
-        e.target.classList.add('active')
-        if(e.target.dataset.background == 'yes'){
-
-            BackgroundOption = true
-            randomizeImgs()
-
-            localStorage.setItem('background-option', true )
-        }else{
-            BackgroundOption = false
-            clearInterval(theInterval)
-
-            localStorage.setItem('background-option', false )
-        }
-
-        
- 
+randomBackGroundEl.forEach((button) => {
+  // click on every button
+  button.addEventListener("click", (e) => {
+    //remove active class from all buttons
+    e.target.parentElement.querySelectorAll(".active").forEach((element) => {
+      element.classList.remove("active");
     });
 
-    
-})
+    // add active class on self
+    e.target.classList.add("active");
+    if (e.target.dataset.background == "yes") {
+      BackgroundOption = true;
+      randomizeImgs();
 
+      localStorage.setItem("background-option", true);
+    } else {
+      BackgroundOption = false;
+      clearInterval(theInterval);
 
-
-
+      localStorage.setItem("background-option", false);
+    }
+  });
+});
 
 // random background option
-function randomizeImgs(){
-    if (BackgroundOption == true){
-        // random background color
-        // select landing page element
-        let landingPage = document.querySelector('.home')
+function randomizeImgs() {
+  if (BackgroundOption == true) {
+    // random background color
+    // select landing page element
+    let landingPage = document.querySelector(".home");
 
-        // get array of images 
-        let imgArray = ['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','10.jpg','12.jpg','13.jpg']
+    // get array of images
+    let imgArray = [
+      "1.jpg",
+      "2.jpg",
+      "3.jpg",
+      "4.jpg",
+      "5.jpg",
+      "6.jpg",
+      "7.jpg",
+      "8.jpg",
+      "9.jpg",
+      "10.jpg",
+      "12.jpg",
+      "13.jpg",
+    ];
 
-        // get random number
-        theInterval = setInterval(() => {
+    // get random number
+    theInterval = setInterval(() => {
+      let randomNumber = Math.floor(Math.random() * imgArray.length + 1);
 
-            let randomNumber = Math.floor(Math.random() * imgArray.length+1)
-            
-            // change background-image url
-            landingPage.style.backgroundImage = `url("images/${randomNumber}.jpg")`
-            
-        }, 3000);
-    }     
-
+      // change background-image url
+      landingPage.style.backgroundImage = `url("images/${randomNumber}.jpg")`;
+    }, 10000);
+  }
 }
-randomizeImgs()
-       
-        
+randomizeImgs();
 
 // digital clock
 
-function clock(){
+function clock() {
+  let date = new Date();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+  let flag = "AM";
 
-    let date = new Date()
-    let hours = date.getHours()
-    let minutes = date.getMinutes()
-    let seconds = date.getSeconds()
-    let flag = "AM"
-    
-    if(hours == 0){
+  if (hours == 0) {
+    hours = 12;
+  }
+  if (hours > 12) {
+    hours = hours - 12;
+    flag = "PM";
+  }
+  if (hours < 10) hours = "0" + hours;
+  if (minutes < 10) minutes = "0" + minutes;
+  if (seconds < 10) seconds = "0" + seconds;
 
-        hours = 12
-    }
-    if(hours > 12){
+  document.querySelector(
+    ".clock"
+  ).innerHTML = `${hours}:${minutes}:${seconds}:${flag}`;
 
-        hours = hours - 12
-        flag = "PM"
-    }
-    if(hours < 10) hours = '0' + hours
-    if(minutes < 10) minutes = '0' + minutes
-    if(seconds < 10) seconds = '0' + seconds
-
-    document.querySelector('.clock').innerHTML = `${hours}:${minutes}:${seconds}:${flag}`
-
-    setTimeout( function() {
-
-        clock()
-        
-    },1000);
-    
-}  
-clock()
-
-
+  setTimeout(function () {
+    clock();
+  }, 1000);
+}
+clock();
 
 // skills progress
-let ourSkills = document.querySelector('.skills')
+let ourSkills = document.querySelector(".skills");
 
-window.onscroll = function(){
+window.onscroll = function () {
+  // skills offset top
+  let skillsOffsetTop = ourSkills.offsetTop;
 
-    // skills offset top
-    let skillsOffsetTop = ourSkills.offsetTop
+  // skills outer hight
+  let skillsOuterHight = ourSkills.offsetHeight;
 
-    // skills outer hight
-    let skillsOuterHight = ourSkills.offsetHeight
+  // window height
+  let windowHeight = this.innerHeight;
 
-    // window height
-    let windowHeight = this.innerHeight
+  //window scrollTop
+  let windowScrollTop = this.pageYOffset;
 
-    //window scrollTop
-    let windowScrollTop = this.pageYOffset
+  if (windowScrollTop > skillsOffsetTop + skillsOuterHight - windowHeight) {
+    let allSkills = document.querySelectorAll(".skills span");
 
-    if (windowScrollTop > (skillsOffsetTop + skillsOuterHight - windowHeight )){
-
-        let allSkills = document.querySelectorAll('.skills span')
-        
-        allSkills.forEach(skill =>{
-
-            skill.style.width = skill.dataset.progress
-        })
-    }
-}
-
-
-
+    allSkills.forEach((skill) => {
+      skill.style.width = skill.dataset.progress;
+    });
+  }
+};
 
 /////// hide
 
-let box = document.querySelectorAll('.box .card')
-let skill = document.querySelectorAll('.skills .skill-box')
+let box = document.querySelectorAll(".box .card");
+let skill = document.querySelectorAll(".skills .skill-box");
 
+document.addEventListener("scroll", function () {
+  box.forEach((card) => {
+    if (isINView(card)) {
+      card.classList.add("visible");
+    }
+  });
 
-document.addEventListener('scroll', function(){
+  skill.forEach((box) => {
+    if (isINView(box)) {
+      box.classList.add("visible");
+    }
+  });
+});
 
-    box.forEach(card =>{
-
-        if(isINView(card)){
-
-            card.classList.add('visible')
-        }
-    })
-
-    skill.forEach(box =>{
-
-        if(isINView(box)){
-
-            box.classList.add('visible')
-            
-        }
-    })
-
-   
-})
-
-
-function isINView(element){
-
-    const rect = element.getBoundingClientRect();
-    return rect.bottom > 0 && rect.top < (window.innerHeight - 100) 
+function isINView(element) {
+  const rect = element.getBoundingClientRect();
+  return rect.bottom > 0 && rect.top < window.innerHeight - 100;
 }
-
-
-
-  
-  
-  
